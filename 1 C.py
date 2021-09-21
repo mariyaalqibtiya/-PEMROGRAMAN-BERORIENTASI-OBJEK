384d1cb9e620a9453f33bee50ba265c865418470
mariyaalqibtiya <90375951+mariyaalqibtiya@users.noreply.github.com>
2021-09-21 14:37:09 +0700

    Add files via upload

---
+++ 1 C.py
@@ -0,0 +1,8 @@
+print ("Menghitung Volume Balok")
+
+    p = float(input("Masukkan panjang balok: "))
+    l = float(input("Masukkan lebar balok: "))
+    t = float(input("Masukan tinggi balok: "))
+    
+Volume = p*l*t
+print("Volume balok adalah : "+ str(Volume))
---
+++ 1 D.py
@@ -0,0 +1,6 @@
+print ("Menghitung Luas Segitiga")
+    a = float(input("Masukkan panjang alas: "))
+    t = float(input("Masukkan tinggi segitiga: "))
+
+luas = 0.5*a*t
+print("Luas segitiga adalah : "+ str(luas))
